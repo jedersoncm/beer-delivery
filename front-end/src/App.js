ddc3c4c0391d471bf6de9771dbd81d3ab83847cc
jederson <jmacedo.ti@gmail.com>
2021-03-28 14:50:03 -0300

    Trybeer

---
+++ front-end/src/App.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Provider from './context/Provider';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import './App.css';
+import ClientProfile from './pages/ClientProfile';
+import Products from './pages/Products';
+import Orders from './pages/Orders';
+import Checkout from './pages/Checkout';
+import AdminOrders from './pages/AdminOrders';
+import AdminProfile from './pages/AdminProfile';
+import ProductDetails from './pages/ProductDetails';
+import OrderDetails from './pages/OrderDetails';
+
+function App() {
+  return (
+    <Provider>
+      <Switch>
+        <Route exact path="/" component={ Login } />
+        <Route exact path="/login" component={ Login } />
+        <Route exact path="/register" component={ Register } />
+        <Route exact path="/profile" component={ ClientProfile } />
+        <Route exact path="/products" component={ Products } />
+        <Route exact path="/checkout" component={ Checkout } />
+        <Route exact path="/orders" component={ Orders } />
+        <Route exact path="/admin/orders" component={ AdminOrders } />
+        <Route exact path="/admin/orders/:id" component={ OrderDetails } />
+        <Route exact path="/admin/profile" component={ AdminProfile } />
+        <Route exact path="/orders/:id" component={ ProductDetails } />
+      </Switch>
+    </Provider>
+  );
+}
+
+export default App;
